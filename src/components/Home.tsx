b851484d4e5175d99ac174de23c5c4724f6fa4c3
Kexy4958 <knighttkey@gmail.com>
2022-09-06 15:04:56 +0800

    use firebase

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,6 +6,10 @@ import React, {
   useEffect,
 } from "react";
 import "./../styles/Home.scss";
+import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage, ref, getDownloadURL, listAll } from "firebase/storage";
 
 type Props = {};
 
@@ -22,9 +26,58 @@ export default (props: Props) => {
   const [context, setContext] = useState<any>();
   const [imgObj, setImgObj] = useState<any>();
   const frameCount = 122;
+  // console.log("ref", ref);
+  const firebaseConfig = {
+    apiKey: "AIzaSyAT6HulJrCrNuuVzRUXqNF5flg8CuM746I",
+    authDomain: "scrollingframes.firebaseapp.com",
+    projectId: "scrollingframes",
+    storageBucket: "scrollingframes.appspot.com",
+    messagingSenderId: "1148924997",
+    appId: "1:1148924997:web:bb1c9af91d94de1986df2c",
+    measurementId: "G-9GJJ48DWKH",
+  };
+  // const database = firebase.database();
+  // const database = firebase.database();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  // const storage = getStorage(app);
+  // console.log("storage", storage);
+  const storage = getStorage(app);
+  console.log("storage", storage);
+  const storageRef = ref(storage);
+  console.log("storageRef", storageRef);
+  const imagesRef = ref(storageRef, "images");
+  console.log("imagesRef", imagesRef);
+  const fileName = "galaxy-001.jpg";
+  const spaceRef = ref(imagesRef, fileName);
+  const path = spaceRef.fullPath;
+  console.log("path", path);
+  const listRef = ref(storage, "images");
+  console.log("listRef", listRef);
+  // var storageRef = firebase.storage().ref("your_folder");
+  useEffect(() => {
+    // Find all the prefixes and items.
+    listAll(storageRef)
+      .then((res) => {
+        console.log("res", res);
+        res.prefixes.forEach((folderRef) => {
+          console.log("folderRef", folderRef);
+          // All the prefixes under listRef.
+          // You may call listAll() recursively on them.
+        });
+        res.items.forEach((itemRef) => {
+          console.log("itemRef", itemRef);
+          // All the items under listRef.
+        });
+      })
+      .catch((error) => {
+        // Uh-oh, an error occurred!
+      });
+  }, [storageRef]);
 
   const currentFrame = (index: number) => {
-    return `galaxy/ga/galaxy-${index.toString().padStart(3, "0")}.jpg`;
+    return `galaxy/galaxy-${index.toString().padStart(3, "0")}.jpg`;
   };
 
   useEffect(() => {
@@ -77,6 +130,7 @@ export default (props: Props) => {
       <div className="wrap">
         <div className="mask">
           <canvas className="canvas" id="canvas"></canvas>
+          {/* <img src="https://firebasestorage.googleapis.com/v0/b/scrollingframes.appspot.com/o/images%2Fgalaxy-001.jpg?alt=media&token=fcd5993a-767d-44fc-883c-42d409ec48a7"></img> */}
         </div>
       </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import firebasePlugin from "vite-plugin-firebase";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir:'docs'
+    outDir: "docs",
   },
-  base: './',
+  base: "./",
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 9001,
   },
-})
+});
